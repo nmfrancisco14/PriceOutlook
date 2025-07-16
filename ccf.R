11f25ef0fdfa73641421da90ea1828d9c382014f
nmfrancisco14 <nefriendfrancisco@gmail.com>
2025-07-16 18:54:04 +0800

    Add CCF analysis script, custom CSS, and stock data
    Added ccf.R for cross-correlation and Granger causality analysis, custom-styles.css for UI enhancements, and external_data/stocks.csv and stocks.rds for inventory data. Updated .gitignore to exclude build artifacts and output files. Minor changes made to priceoutlook.qmd.

---
+++ ccf.R
@@ -0,0 +1,32 @@
+ccf(data_edited$ipp_ave, data_edited$retail_prices |> filter(type=="rmr"), lag.max = 12)
+
+
+
+
+ippave_ts <- ts(data_edited$ipp_ave$price, start = c(2018,7), frequency = 12)
+
+pricedata <- 
+data_edited$ipp_ave |> 
+  rename(ipp=price) |> 
+  left_join(data_edited$retail_prices |> filter(type=="rmr") |> select(date,rprice=price)) |> 
+  
+  left_join(data_edited$wholesale_prices |> filter(type=="rmr") |> select(date,wprice=price)) |> 
+  
+  left_join(data_edited$farmgate_prices |> select(date,farmgate)) |> 
+  left_join(data_edited$ipp |> filter(type=="Vietnam 25%") |> select(date,viet_price=price)) 
+
+
+ccf(pricedata$viet_price,pricedata$rprice, lag.max=12)
+
+
+library(tseries)
+adf.test(pricedata$farmgate)
+
+ccf(pricedata$rprice,pricedata$farmgate, lag.max=12)
+
+
+library(lmtest)
+
+grangertest(rprice ~ farmgate, order = 6, data = pricedata)
+
+grangertest(ipp~rprice, order = 2, data = pricedata)
